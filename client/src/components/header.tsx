f4ed5180daefb89fc002204455ffd8aa42834245
Kamal Singh <itskamal.dev@gmail.com>
2024-12-08 10:51:24 +0530

    changed logo

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
   return (
     <header className="sticky h-14 w-full px-5 lg:px-10 pt-2 inline-flex items-center gap-5 text-white">
       <a href="/" className=" relative w-fit h-full flex gap-3 items-center">
-        <img src="/logo.jpeg" alt="Logo" className="relative h-full rounded-sm" />
+        <img src="/logo.png" alt="Logo" className="relative h-full rounded-sm p-1" />
         <h1 className="text-lg font-semibold">Dashboard</h1>
       </a>
     </header>
